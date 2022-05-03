d197aef9c343a28dd560ec3b63e981b939729c8f
Marcin Goryca <marcin.goryca@gmail.com>
2022-05-03 20:27:57 +0200

    update

---
+++ basic/jessy/js/console.js
@@ -0,0 +1,9 @@
+( function() {
+    "use strict";
+
+    let a = 'Marianna';
+    let b = 'Leo';
+
+    console.log('%cNames', 'color: red; font-weight: bold; font-decoration: underline');
+    console.log({a, b});
+})();
